4c69070d5488dafc5c02ca9e190eff3d01079b4e
wanandas <wanzerox@gmail.com>
2019-06-29 02:22:42 +0700

    start from 1st page and move icon btn

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -58,10 +58,11 @@ const renderRecipe = recipe => {
 const createButton = (page, type) =>  
         `
         <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1 }>
+                <span>Page${type === 'prev' ? page - 1 : page + 1 }</span>
                 <svg class="search__icon">
                     <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left': 'right' }"></use>
                 </svg>
-            <span>Page${type === 'prev' ? page - 1 : page + 1 }</span>
+           
         </button>
         `
     
@@ -89,7 +90,7 @@ const renderButtons = (page, numResults, resPerPage) => {
     elements.searchResPage.insertAdjacentHTML('afterbegin', button)
 }
 
-export const renderResults = (recipes, page = 2, resPerPage = 10) => { 
+export const renderResults = (recipes, page = 1, resPerPage = 10) => { 
     // render results of current page
     const start = (page - 1 ) * resPerPage
     const end = page * resPerPage
