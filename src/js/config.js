e108cec7236cb064528c5e27f9408d33af6afec2
wanandas <wanzerox@gmail.com>
2019-06-30 19:42:21 +0700

    Update servings btn

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,3 @@
-export const key = '631fdec2374475f067d926a3c4cd7820'
+//export const key = '631fdec2374475f067d926a3c4cd7820'
 //export const key = 'fec3d56ebdd55b8a4429859ab7b70ec8'
+export const key = '462b1cc8d4f2730081462fbc65136320'
